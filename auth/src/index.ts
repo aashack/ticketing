29a9fd4d6cf4ea07d61488469b5a9fe79b8a2714
Aaron Shack <aashack@gmail.com>
2023-09-11 14:53:05 -0700

    section 140

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,7 +5,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-
+import { errorHandler } from './middlewares/error-handlers';
 
 const app = express();
 app.use(json());
@@ -15,6 +15,7 @@ app.use(signinRouter);
 
 app.use(signoutRouter);
 app.use(signupRouter);
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log('Aaron Was Here');
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -7,14 +7,17 @@ router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be Valid'),
     body('password').trim().isLength({ min: 4, max: 20}).withMessage('Password must be between 4 and 20 characters')
 ], (req: Request, res: Response) => {
+    
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
-        return res.status(400).send(errors.array())
+        throw new Error('Invalid email or Password');
     }
 
 
     const {email, password } = req.body;
     console.log('Creating a user...');
+    throw new Error('Error connecting to Database');
+
 
     res.send({});
 
