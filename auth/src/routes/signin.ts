3206cc072023892ff3b41d06ddd713bc78430cce
Aaron Shack <aashack@gmail.com>
2023-10-09 14:54:02 -0700

    NEXT you need to add next in middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,43 +1,58 @@
-import express, { Request, Response} from 'express';
+import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { Password } from '../services/password';
+import { User } from '../models/user';
 import { validateRequest } from '../middlewares/validate-request';
 import { BadRequestError } from '../errors/bad-request-error';
-import { User } from '../models/user';
-import { Password } from '../services/password';
-import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.post('/api/users/signin', [
-    body("email").isEmail().withMessage("Email must be valid"),
-    body("password")
+router.post(
+  '/api/users/signin',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
       .trim()
       .notEmpty()
-      .withMessage("Password must be between 4 and 20 characters"),
-],
-validateRequest,
-async (req: Request, res: Response) => {
+      .withMessage('You must supply a password')
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
-    const existingUser = await User.findOne({email});
+    const existingUser = await User.findOne({ email });
     if (!existingUser) {
-        throw new BadRequestError('Invalid Credentials');
+      throw new BadRequestError('Invalid credentials');
     }
 
-    const passwordsMatch = await Password.compare(existingUser.password, password);
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
     if (!passwordsMatch) {
-        throw new BadRequestError('Invalid Credentials');
+      throw new BadRequestError('Invalid Credentials');
     }
-    const userJwt = jwt.sign({
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
         id: existingUser.id,
         email: existingUser.email
-      }, process.env.JWT_KEY!);
-  
-      req.session = {
-        jwt: userJwt
-      };
+      },
+      process.env.JWT_KEY!
+    );
 
-      res.status(200).send(existingUser);
-});
+    // Store it on session object
+    req.session = {
+      jwt: userJwt
+    };
 
-export {router as signinRouter };
+    res.status(200).send(existingUser);
+  }
+);
+
+export { router as signinRouter };
